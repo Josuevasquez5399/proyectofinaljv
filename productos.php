06cde6356b94cebfe8f33d32c84fbe2162983665
Josuevasquez5399 <jg2800988@gmail.com>
2021-08-24 19:29:50 -0600

    usted ha hecho un cambio al body

---
+++ productos.php
@@ -0,0 +1,198 @@
+<!doctype html>
+<html lang="en">
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="fondo2.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+
+    <title>productos</title>
+  </head>
+  <body>
+    <br><br><br>
+    <div class="container">
+        <div class="btn-group w-100" role="group" aria-label="Basic exaple">
+        <button type="button" class="btn btn-outline-warning"><a href="index.html">Inicio</a></button>
+        <button type="button" class="btn btn-outline-info"><a href="tiendas.php">Tiendas</a></button>
+        <button type="button" class="btn btn-outline-primary"><a href="contactenos.php">Contactenos</a></button>
+    </div>
+    </div>
+    <div class="container">
+    <h2>Productos y servicios</h2>
+  </div>
+   <br><br>
+   <div class="conteiner">
+    <div class="container">
+        <div class="row">
+
+          <div class="col-3" align="center">
+            <div class="card" style="width: 15rem; background-color:hsla(295, 100%, 43%, 0.192)">
+                <img src="imagenes/pro1.png" class="card-img-top" height="237">
+                <div class="card-body">
+                  <h5 class="card-title">Camperitos y alitas</h5>
+                  <p class="card-text"><b></b> | Q145.00 |</p>
+                  <a  class="btn btn-danger">Mas Informacion</a>
+                  
+                </div>
+              </div>
+          </div><br>
+
+          <div class="col-3" align="center">
+            <div class="card" style="width: 15rem; background-color:hsla(295, 100%, 43%, 0.192)">
+                <img src="imagenes/pro2.png" class="card-img-top" height="215">
+                <div class="card-body">
+                  <h5 class="card-title">Camperitos Doritos Flamin Hot</h5>
+                  <p class="card-text"><b></b>| Q95.50 |</p>
+                  <a class="btn btn-danger">Mas Informacion</a>
+                </div>
+              </div>
+          </div><br>
+          <div class="col-3" align="center">
+            <div class="card" style="width: 15rem; background-color:hsla(295, 100%, 43%, 0.192)">
+                <img src="imagenes/pro3.png" class="card-img-top" height="215">
+                <div class="card-body">
+                  <h5 class="card-title">Pizza Grande</h5>
+                  <p class="card-text"><b>  </b>| Q165.00 |</p>
+                  <a class="btn btn-danger">Mas Informacion</a>
+                </div>
+              </div>
+          </div><br>
+
+          <div class="col-3" align="center">
+            <div class="card" style="width: 15rem; background-color:hsla(295, 100%, 43%, 0.192)">
+                <img src="imagenes/pro4.png" class="card-img-top" height="237">
+                <div class="card-body">
+                  <h5 class="card-title">Pollo Bañado</h5>
+                  <p class="card-text"><b> </b>| Q80.95 |</p>
+                  <a class="btn btn-danger">Mas Informacion</a>
+                </div>
+              </div>
+          </div><br>
+        </div>
+    </div>
+</div>
+<br><br><br>
+<div class="conteiner">
+    <div class="container">
+        <div class="row">
+
+          <div class="col-3" align="center">
+            <div class="card" style="width: 15rem; background-color:hsla(295, 100%, 43%, 0.192)">
+                <img src="imagenes/pro5.png" class="card-img-top" height="237">
+                <div class="card-body">
+                  <h5 class="card-title"> Sándwiches </h5>
+                  <p class="card-text"><b></b>| Q50.00 |</p>
+                  <a class="btn btn-danger">Mas Informacion</a>
+                </div>
+              </div>
+          </div><br>
+
+          <div class="col-3" align="center">
+            <div class="card" style="width: 15rem; background-color:hsla(295, 100%, 43%, 0.192)">
+                <img src="imagenes/pro6.png" class="card-img-top" height="237">
+                <div class="card-body">
+                  <h5 class="card-title"> Hamburguesas </h5>
+                  <p class="card-text"><b> </b>| Q55.00 |</p>
+                  <a class="btn btn-danger">Mas Informacion</a>
+                </div>
+              </div>
+          </div><br>
+          <div class="col-3" align="center">
+            <div class="card" style="width: 15rem; background-color:hsla(295, 100%, 43%, 0.192)">
+                <img src="imagenes/pro7.png" class="card-img-top" height="215">
+                <div class="card-body">
+                  <h5 class="card-title"> Burritos </h5>
+                  <p class="card-text"><b></b>| Q65.50 |</p>
+                  <a class="btn btn-danger">Mas Informacion</a>
+                </div>
+              </div>
+          </div><br>
+
+          <div class="col-3" align="center">
+            <div class="card" style="width: 15rem; background-color:hsla(295, 100%, 43%, 0.192)">
+                <img src="imagenes/pro8.png" class="card-img-top" height="237">
+                <div class="card-body">
+                  <h5 class="card-title"> Ensaladas y Tazón </h5>
+                  <p class="card-text"><b></b>| Q95.30|</p>
+                  <a  class="btn btn-danger">Mas Informacion</a>
+                </div>
+              </div>
+          </div><br>
+        </div>
+    </div>
+</div>
+<br><br><br>
+<div class="conteiner">
+    <div class="container">
+        <div class="row">
+
+          <div class="col-3" align="center">
+            <div class="card" style="width: 15rem; background-color:hsla(295, 100%, 43%, 0.192)">
+                <img src="imagenes/pro9.png" class="card-img-top" height="237">
+                <div class="card-body">
+                  <h5 class="card-title">Cenas</h5>
+                  <p class="card-text"><b></b>| Q60.00 | </p>
+                  <a class="btn btn-danger">Mas Informacion</a>
+                </div>
+              </div>
+          </div><br>
+
+          <div class="col-3" align="center">
+            <div class="card" style="width: 15rem; background-color:hsla(295, 100%, 43%, 0.192)">
+                <img src="imagenes/pro10.png" class="card-img-top" height="215">
+                <div class="card-body">
+                  <h5 class="card-title">Loncherita</h5>
+                  <p class="card-text"><b></b>| Q45.00 |</p>
+                  <a  class="btn btn-danger">Mas Informacion</a>
+                </div>
+              </div>
+          </div><br>
+          <div class="col-3" align="center">
+            <div class="card" style="width: 15rem; background-color:hsla(295, 100%, 43%, 0.192)">
+                <img src="imagenes/pro11.png" class="card-img-top" height="215">
+                <div class="card-body">
+                  <h5 class="card-title">Desayunos</h5>
+                  <p class="card-text"><b></b>| Q80.50 |</p>
+                  <a class="btn btn-danger">Mas Informacion</a>
+                </div>
+              </div>
+          </div><br>
+
+          <div class="col-3" align="center">
+            <div class="card" style="width: 15rem; background-color:hsla(295, 100%, 43%, 0.192)">
+                <img src="imagenes/pro12.png" class="card-img-top" height="237">
+                <div class="card-body">
+                  <h5 class="card-title">Postres</h5>
+                  <p class="card-text"><b></b>| Q25.00 |</p>
+                  <a  class="btn btn-danger">Mas Informacion</a>
+                </div>
+              </div>
+          </div><br>
+        </div>
+    </div>
+</div>
+<br>
+     
+ </body>
+</html>
+
+   <br><br>   <div class="container">
+    <table border="1" width="100%" >
+      <tr>
+        <td bgcolor="#05A0FF">
+          ........PBX: 55968882.............Dirrecion: zona-12 colonia- La Reformita ..................................................................................Carnet:2017-40260 .........Nombre: Josue David Gomez Vasquez
+        </td>
+      </tr>
+  </div>
+  </table>
+  <br>
+  <br>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+
+    
+  </body>
+</html> 
---
+++ tiendas.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="fondo3.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+
+    <title>Tiendas</title>
+  </head>
+  <body>
+      <br><br>
+
+    <div class="container">
+        <div class="btn-group w-100" role="group" aria-label="Basic exaple">
+        <button type="button" class="btn btn-outline-warning"><a href="index.html">Inicio</a></button>
+        <button type="button" class="btn btn-outline-info"><a href="productos.php">Productos/Servicios</a></button>
+        <button type="button" class="btn btn-outline-primary"><a href="contactenos.php">Contactenos</a></button>
+    </div>
+    </div>
+    <br>
+    <div class="container">
+    <h2>Nuestras Tiendas</h2>
+  </div>
+  <div class="container">
+    <div class="container">
+           <div class="row">
+             <img src="imagenes/res1.jpg"class="img thumnail" style="height: 505px; width: 600px;">
+             </div>
+   </div>
+   <br>
+   <div class="container">
+    <div class="container">
+           <div class="row">
+             <img src="imagenes/res2.jpg"class="img thumnail" style="height: 505px; width: 600px;">
+             </div>
+   </div>
+   <br>
+   <div class="container">
+    <div class="container">
+           <div class="row">
+             <img src="imagenes/res3.jpg"class="img thumnail" style="height: 505px; width: 600px;">
+             </div>
+   </div>
+  </body>
+</html> 
